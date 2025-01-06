e7895c4f9e6f08a92740b1600b6b72466e017734
faustinopsy <rodrigohipnose@gmail.com>
2025-01-06 14:20:50 -0300

    criando uma tabelas para iniciar testes

---
+++ database.sql
@@ -0,0 +1,14 @@
+CREATE TABLE `users` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `firstName` varchar(255) DEFAULT NULL,
+  `lastName` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `isAdmin` tinyint DEFAULT '0',
+  `two_fa_code` varchar(6) DEFAULT NULL,
+  `two_fa_expires_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
