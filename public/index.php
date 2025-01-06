bf852bbc54ec6e84629449626223fc8d370a28ba
faustinopsy <rodrigohipnose@gmail.com>
2025-01-06 14:08:35 -0300

    iniciando conexão com banco de dados

---
+++ public/index.php
@@ -0,0 +1,3 @@
+<?php
+
+require './iniciar.php';
---
+++ public/iniciar.php
@@ -0,0 +1,24 @@
+<?php
+
+use app\database\Connection;
+
+require __DIR__.'/../vendor/autoload.php';
+
+session_start();
+
+ini_set('memory_limit', '1024M');
+set_time_limit(0);
+ini_set('display_errors', 1);
+ini_set('log_errors', 1);
+ini_set('error_log', __DIR__ . '/logs/php_errors.log');
+error_reporting(E_ALL);
+ 
+if (!file_exists(__DIR__ . '/logs')) {
+   mkdir(__DIR__ . '/logs', 0755, true);
+}
+
+$dotenv = Dotenv\Dotenv::createImmutable(dirname(__FILE__, 2));
+$dotenv->load();
+
+// $conexao = Connection::connect();
+// var_dump($conexao);
